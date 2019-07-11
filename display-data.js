b7ed6ffd193b569b950f28ebb0f672a0aa4590cc
Carlisle Miller <carlisle.m@hotmail.com>
2019-07-10 18:12:53 -0700

    Test function

--- display-data.js
+++ display-data.js
@@ -1,23 +1,51 @@
+// function getData() {
+
+//     var pg = require('pg');
+
+//     var connectionString = "postgres://djpoucmhkewvrh:e1a533e45aa586bf82ff18dcc021969e6fb438333e501973f5236ab9257aea9c@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d24ubplectbqas";
+
+//     console.log("trying to connect");
+
+//     var pgClient = new pg.Client(connectionString);
+
+//     pgClient.connect();
+
+//     console.log(connected);
+
+//     var query = pgClient.query("SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id");
+
+//     query.on("row", function(row,result){
+//         result.addRow(row);
+//     });
+
+//     console.log(results);
+
+// }
+
 function getData() {
 
-    var pg = require('pg');
+    const { Pool, Client } = require('pg')
+    const connectionString = 'postgres://djpoucmhkewvrh:e1a533e45aa586bf82ff18dcc021969e6fb438333e501973f5236ab9257aea9c@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d24ubplectbqas'
 
-    var connectionString = "postgres://djpoucmhkewvrh:e1a533e45aa586bf82ff18dcc021969e6fb438333e501973f5236ab9257aea9c@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d24ubplectbqas";
+    const pool = new Pool({
+        connectionString: connectionString,
+    })
 
-    console.log("trying to connect");
+    pool.query('SELECT NOW()', (err, res) => {
+        console.log(err, res)
+        pool.end()
+    })
 
-    var pgClient = new pg.Client(connectionString);
+    const client = new Client({
+        connectionString: connectionString,
+    })
+    client.connect()
 
-    pgClient.connect();
+    client.query('SELECT NOW()', (err, res) => {
+        console.log(err, res)
+        client.end()
+    })
 
-    console.log(connected);
-
-    var query = pgClient.query("SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id");
-
-    query.on("row", function(row,result){
-        result.addRow(row);
-    });
-
-    console.log(results);
+    console.log('test');
 
 }
