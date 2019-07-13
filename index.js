829e0970f3ab1c6816a52756903315a6709b1328
Carlisle Miller <carlisle.m@hotmail.com>
2019-07-13 12:36:08 -0700

    Test

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
+// const cool = require('cool-ascii-faces')
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static('public'))
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .get('/about', (req, res) => res.send(showTimes()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// // testFunc();
+// // getData();
+
+// showTimes = () => {
+//   let result = ''
+//   const times = process.env.TIMES || 5
+//   for (i = 0; i < times; i++) {
+//     result += i + ' '
+//   }
+//   return result;
+// }
+
+
 const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const path = require('path');
 
-express()
-  .use(express.static('public'))
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/about', (req, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-// testFunc();
-// getData();
-
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
-
+app.listen(process.env.PORT || 5000, function(){
+    console.log('Your node js server is running');
+});
