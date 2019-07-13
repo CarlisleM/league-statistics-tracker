fa6affd2b78b3048a62f3a14e9d38df432636fa1
Carlisle Miller <carlisle.m@hotmail.com>
2019-07-13 13:20:14 -0700

    Deleting duplicate files

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-// const cool = require('cool-ascii-faces')
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// express()
-//   .use(express.static('public'))
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .get('/cool', (req, res) => res.send(cool()))
-//   .get('/about', (req, res) => res.send(showTimes()))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-// // testFunc();
-// // getData();
-
-// showTimes = () => {
-//   let result = ''
-//   const times = process.env.TIMES || 5
-//   for (i = 0; i < times; i++) {
-//     result += i + ' '
-//   }
-//   return result;
-// }
-
-
-const cool = require('cool-ascii-faces')
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 5000, function(){
-    console.log('Your node js server is running');
-});
+express()
+  .use(express.static('public'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
