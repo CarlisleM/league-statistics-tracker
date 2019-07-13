e1089a6a1ee249a9cb099c76d0124de27dbd6f82
Carlisle Miller <carlisle.m@hotmail.com>
2019-07-13 13:37:44 -0700

    Test connection

--- display-data.js
+++ display-data.js
@@ -1,28 +1,28 @@
-function getData() {
+// function getData() {
 
-    console.log("test connect");
+//     console.log("test connect");
 
-    var pg = require('pg');
+//     var pg = require('pg');
 
-    var connectionString = "postgres://djpoucmhkewvrh:e1a533e45aa586bf82ff18dcc021969e6fb438333e501973f5236ab9257aea9c@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d24ubplectbqas";
+//     var connectionString = "postgres://djpoucmhkewvrh:e1a533e45aa586bf82ff18dcc021969e6fb438333e501973f5236ab9257aea9c@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d24ubplectbqas";
 
-    console.log("trying to connect");
+//     console.log("trying to connect");
 
-    var pgClient = new pg.Client(connectionString);
+//     var pgClient = new pg.Client(connectionString);
 
-    pgClient.connect();
+//     pgClient.connect();
 
-    console.log(connected);
+//     console.log(connected);
 
-    var query = pgClient.query("SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id");
+//     var query = pgClient.query("SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id");
 
-    query.on("row", function(row,result){
-        result.addRow(row);
-    });
+//     query.on("row", function(row,result){
+//         result.addRow(row);
+//     });
 
-    console.log(result);
+//     console.log(result);
 
-}
+// }
 
 // function getData() {
 // conn = psycopg2.connect(user = "djpoucmhkewvrh", password = "e1a533e45aa586bf82ff18dcc021969e6fb438333e501973f5236ab9257aea9c", host = "ec2-174-129-209-212.compute-1.amazonaws.com", port = "5432", database = "d24ubplectbqas", sslmode = 'require')
