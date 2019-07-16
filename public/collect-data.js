5a8368337c3db095b439881730bc5c232ff4ab8c
Carlisle Miller <carlisle.m@hotmail.com>
2019-07-16 03:45:30 -0700

    Fix connection issue

--- public/collect-data.js
+++ public/collect-data.js
@@ -10,30 +10,24 @@ var pgClient = new pg.Client({
     ssl: true
 }); 
 
-
 function getData(callback) {
 
     pgClient.connect();
     
-    pgClient.query('SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id', (err, result) => {
-      
-      pgClient.end();
-    })
+    var query = pgClient.query("SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id");
 
-    // var query = pgClient.query("SELECT game_date, game_count, blue_team, red_team, first_blood, first_tower, first_dragon, first_inhibitor, first_baron, winner FROM games, match_data where id = game_id");
+    query.on("row", function (row, result) {
+        result.addRow(row);
+    });
 
-    // query.on("row", function (row, result) {
-    //     result.addRow(row);
-    // });
+    query.on("end", function (result) {
+     // callback(result.rows, null, "    ");
+        callback(result.rows, null);
+     // callback(JSON.stringify(result.rows, null));
+     // console.log(JSON.stringify(result.rows, null, "    "));
+    });
 
-    // query.on("end", function (result) {
-    //  // callback(result.rows, null, "    ");
-    //     callback(result.rows, null);
-    //  // callback(JSON.stringify(result.rows, null));
-    //  // console.log(JSON.stringify(result.rows, null, "    "));
-    // });
-
-    // pgClient.end();
+ //   pgClient.end();
 
 }
 
