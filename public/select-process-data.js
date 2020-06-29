19395f154af44c20cf3f69eb12e8311dbf3b36a0
Carlisle Miller <carlisle.m@hotmail.com>
2020-06-28 17:12:08 -0700

    Moving upcoming games to the sidebar

--- public/select-process-data.js
+++ public/select-process-data.js
@@ -291,8 +291,7 @@ function change_team_options(leaguename) {
             if (data.upcoming_matches[k].league_id == current_league) {
                 // Upcoming match div
                 var match = document.createElement("div");
-                //match.setAttribute('class', 'match_row');
-                match.setAttribute('class', 'sideBar');
+                match.setAttribute('class', 'match_row');
                 match.id = '' + data.upcoming_matches[k].blue_team + ' ' + data.upcoming_matches[k].red_team;
 
                 var individual_team_1 = document.createElement("div");
