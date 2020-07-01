f699b3fbe628475e61be17e72bc5b2f8403d2bc1
Carlisle Miller <carlisle.m@hotmail.com>
2020-07-01 05:33:22 -0700

    Fixed a bug that caused upcoming matches on the current day to still display even if their match time had passed

--- index.js
+++ index.js
@@ -58,8 +58,6 @@ app.get('/upcoming', async (req, res) => {
   })
   client.connect()
 
-  (TO_TIMESTAMP('04:00','HH24:MI:SS')::TIME < timezone('PDT', NOW())::TIME)
-
   const result = await client.query("SELECT * FROM upcoming WHERE (game_date = DATE(NOW()) AND (TO_TIMESTAMP(match_time,'HH24:MI:SS')::TIME > timezone('PDT', NOW())::TIME(0))) OR game_date > DATE(NOW());")
   await client.end()
   res.json({ upcoming_matches: result.rows })
